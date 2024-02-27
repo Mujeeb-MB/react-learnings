6a56c6d6ba86bd56f4a4ac3786b72b2ba2dd16e1
mujeeb <md.mujeeb5577@gmail.com>
2024-02-28 02:25:45 +0530

    Buid bg color project

---
+++ bgchanger/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from "react";
+
+useState;
+
+function App() {
+  const [color, setColor] = useState("olive");
+
+  return (
+    <>
+      <div
+        className="w-full h-screen duration-200"
+        style={{ backgroundColor: color }}
+      ></div>
+      <div className="fixed flex flex-wrap justify-center top-10 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-xl">
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white"
+            style={{ backgroundColor: "red" }}
+            onClick={() => setColor("red")}
+          >
+            Red
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white"
+            style={{ backgroundColor: "green" }}
+            onClick={() => setColor("green")}
+          >
+            Green
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white"
+            style={{ backgroundColor: "blue" }}
+            onClick={() => setColor("blue")}
+          >
+            Blue
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white"
+            style={{ backgroundColor: "black" }}
+            onClick={() => setColor("black")}
+          >
+            Black
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white"
+            style={{ backgroundColor: "orange" }}
+            onClick={() => setColor("orange")}
+          >
+            Orange
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-full text-white"
+            style={{ backgroundColor: "purple" }}
+            onClick={() => setColor("purple")}
+          >
+            Purple
+          </button>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
