5270e0dd08553331a2950c88fa2790d488bbd4e3
mujeeb <md.mujeeb5577@gmail.com>
2024-02-27 18:55:42 +0530

    created first component

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+import Mb from "./mb"
+
+function App() {
+
+  const name = "mujeeb"
+
+  return (
+    <>
+    <Mb />
+    <h1>hello mb is {name}</h1>
+    <h1>hello mb</h1>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/mb.jsx
@@ -0,0 +1,7 @@
+function Mb(){
+    return (
+        <h1>this is mb comp..</h1>
+    )
+}
+
+export default Mb
